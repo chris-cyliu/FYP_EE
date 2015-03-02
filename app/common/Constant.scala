8ede9134eff4443714c881f57e111d6279dce06f
Chris <fafaoc@me.com>
2015-03-02 13:31:18 +0800

    Update setting for testing purpose

--- app/common/Constant.scala
+++ app/common/Constant.scala
@@ -8,7 +8,7 @@ object Constant {
   val actor_name_db = "db"
   val actor_name_agg = "agg"
   val tcp_port = 5858
-  val base_url = "http://127.0.0.1:8181"
+  val base_url = "http://127.0.0.1:9000"
 
   val push_interval_second = 5
 }
